841b2f867c1815848bda2c07bfad38d5e4b24266
gianlu01 <gianluca.parpanesi@gmail.com>
2019-12-11 18:07:27 +0100

    Modifica al .gitingore

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ Sviluppato per dare possibilità ai Milanesi di divertirsi e conoscere al meglio
 
 ### Linguaggi utilizzati per il back-end:
 - MongoDB, database in JSON
+
+
 I Dataset utilizzati provengono dal sito del Comune di Milano, esso li mette a disponsizione per poterli utilizzare per svolgere analisi o, come nel nostro caso, sviluppare un appplicazione web.
 
 ### Link ai Dataset:
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from '../icons/logo.png';
 
+
+function Login(){
+  alert("Schiacciato il login");
+}
+
 function Nav() {
   return (
     <nav>
@@ -12,7 +17,7 @@ function Nav() {
             <li><a href="" className="nav-link">Cosa facciamo</a></li>
             <li><a href="" className="nav-link">Contatti</a></li>
             <li><a href="" className="nav-link">Lavora con noi</a></li>
-            <li><a href="" className="nav-link btn-login">Login</a></li>
+            <li><a href="" className="nav-link btn-login" onClick={Login()}>Login</a></li>
         </ul>
         </div>
     </nav>
