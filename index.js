d8ef3cadbcd4e067efd6aa1056916c2a65a16bc8
Michele Banfi <michi.banfi01@gmail.com>
2020-01-14 17:35:40 +0100

    connect backend and cluster

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var db = {};
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect("mongodb+srv://Michele:Arancione6@cluster0-0jqkz.mongodb.net/test?retryWrites=true", { useUnifiedTopology: true }, (err, client) => {
+    db.collection = client.db('test').collection('testdb');
+    if (err) console.log(err);
+});
 
 //this is the default route and send back the content of the react build command
 app.use(express.static(path.join(__dirname, 'build')));
@@ -14,9 +20,16 @@ app.get('/get', (req, res) => {
 });
 
 //post, example request on /post
-app.get('/post', (req, rest) => {
+app.get('/post', (req, res) => {
     res.send("200");
 });
+
+app.get('/mongo', (req, res) => {
+    db.collection.findOne({ property1: 10 }, (err, doc) => {
+        if (err) console.log(err);
+        else console.log(doc);
+    })
+})
 
 //specify the port were listen to
 app.listen(80, () => {
