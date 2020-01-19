f86fdd37a79890c2ed1a3058a49bdabb0adf0474
gianlu01 <gianluca.parpanesi@gmail.com>
2020-01-19 12:04:46 +0100

    Aggiunto Switch nell'index

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,11 @@ import Maps from './Maps.js';
 
 class Header extends React.Component {
 
-  
+
   constructor(props) {
     super(props);
     this.state = {
-      showMaps: false
+      components: "header"
     }
   }
 
@@ -17,45 +17,53 @@ class Header extends React.Component {
 
   render() {
 
-    const header = 
-    
-    (<header>
-      <Nav />
-      <div className="cover">
-        <div className="container">
-          <div className="flex-wrapper">
-            <div className="text-wrapper">
-              <span className="brand">ABOUTMI</span>
-              <h1>Esplora Milano tra Bar, Pub, Ristoranti e Discoteche.</h1>
-              <div className="btn">Visita il sito</div>
-            </div>
-            <div className="card-wrapper">
-              <div className="card">
-                <p>Scopri gli eventi più adatti a te nella tua zona!</p>
-                <div className="btn" onClick = { () => {
-                  this.setState({ showMaps: true});
-                }}
-                >Vedi mappa</div>
+    const maps = (<Maps />);
+    const header =
+
+      (<header>
+        <Nav />
+        <div className="cover">
+          <div className="container">
+            <div className="flex-wrapper">
+              <div className="text-wrapper">
+                <span className="brand">ABOUTMI</span>
+                <h1>Esplora Milano tra Bar, Pub, Ristoranti e Discoteche.</h1>
+                <div className="btn">Visita il sito</div>
+              </div>
+              <div className="card-wrapper">
+                <div className="card">
+                  <p>Scopri gli eventi più adatti a te nella tua zona!</p>
+                  <div className="btn" onClick={() => {
+                    this.setState({ components: "maps" });
+                  }}
+                  >Vedi mappa</div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="scroll-wrapper">
-          <div className="scroll-btn">
-            <div className="line-wrapper">
-              <span className="line"></span>
+          <div className="scroll-wrapper">
+            <div className="scroll-btn">
+              <div className="line-wrapper">
+                <span className="line"></span>
+              </div>
+              <span>Scroll</span>
+              <span>down</span>
             </div>
-            <span>Scroll</span>
-            <span>down</span>
           </div>
         </div>
-      </div>
-      <LoginModal />
-    </header>);
+        <LoginModal />
+      </header>);
 
-    const maps = (<Maps />);
+    switch (this.state.components) {
+      case "header":
+        return(header);
+        break;
+      case "maps":
+        return(maps);
+        break;
+      default:
 
-    return ( this.state.showMaps ? maps : header);
+    }
   }
 
 }
