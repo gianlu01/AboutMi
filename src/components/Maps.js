9deac68cc4b2a3522b67350c290371a03be365bb
Michele Banfi <michi.banfi01@gmail.com>
2020-01-29 13:38:59 +0100

    working map

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -2,9 +2,8 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import ReactMapGL from 'react-map-gl';
 
-//mapboxgl.accessToken = 'pk.eyJ1IjoiZ2lhbmx1MDEiLCJhIjoiY2s1ejQ0a2gyMDY5NjNtcWp5cGF4Y21wMiJ9.S2-22wqQvv8B0aiya-Mh7A';
-
-
+//accessToken: 'pk.eyJ1IjoiZ2lhbmx1MDEiLCJhIjoiY2s1ejQ0a2gyMDY5NjNtcWp5cGF4Y21wMiJ9.S2-22wqQvv8B0aiya-Mh7A';
+//site URL: mapbox://styles/gianlu01/ck5z9olku3d2r1jov9drsa1uu
 
 export default function Maps(){
   const [viewport, setViewport] = useState({
@@ -12,13 +11,21 @@ export default function Maps(){
     height: '100vh',
     latitude: 45.466944,
     longitude: 9.19,
-    zoom: 8
+    zoom: 11
   });
 
       //const Milan = transform([45.466944, 9.19], 'ESPG:4326', 'ESPG:4357');
       return (
         <div>
-        <ReactMapGL   {...viewport} onViewportChange={setViewport} mapboxApiAccessToken={'pk.eyJ1IjoiZ2lhbmx1MDEiLCJhIjoiY2s1ejQ0a2gyMDY5NjNtcWp5cGF4Y21wMiJ9.S2-22wqQvv8B0aiya-Mh7A'}>
+        <ReactMapGL
+        {...viewport}
+        onViewportChange={setViewport}
+        mapboxApiAccessToken={'pk.eyJ1IjoiZ2lhbmx1MDEiLCJhIjoiY2s1ejQ0a2gyMDY5NjNtcWp5cGF4Y21wMiJ9.S2-22wqQvv8B0aiya-Mh7A'}
+        mapStyle={'mapbox://styles/gianlu01/ck5z9olku3d2r1jov9drsa1uu'  }
+        >
+        <div>
+          <input placeholder="Cerca un locale"></input>
+        </div>
         </ReactMapGL>
         </div>
       );
