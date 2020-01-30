9800e0e8e37f31218bc2bafe9a80b460ac5ac59d
Michele Banfi <michi.banfi01@gmail.com>
2020-01-30 18:50:41 +0100

    every commit makes the project more beautiful

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,45 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ReactMapboxGl from "react-mapbox-gl";
+import ReactMapboxGl, {Layer} from "react-mapbox-gl";
 import DrawControl from "react-mapbox-gl-draw";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 
 //accessToken: 'pk.eyJ1IjoiZ2lhbmx1MDEiLCJhIjoiY2s1ejQ0a2gyMDY5NjNtcWp5cGF4Y21wMiJ9.S2-22wqQvv8B0aiya-Mh7A';
 //site URL: mapbox://styles/gianlu01/ck5z9olku3d2r1jov9drsa1uu
 
-/*export default function Maps(){
-  const [viewport, setViewport] = useState({
-    width: '100%',
-    height: '100vh',
-    latitude: 45.466944,
-    longitude: 9.19,
-    zoom: 11
-  });
-
-      //const Milan = transform([45.466944, 9.19], 'ESPG:4326', 'ESPG:4357'); 11
-      return (
-        <div>
-        <ReactMapGL
-        {...viewport}
-        onViewportChange={setViewport}
-        mapboxApiAccessToken={'pk.eyJ1IjoiZ2lhbmx1MDEiLCJhIjoiY2s1ejQ0a2gyMDY5NjNtcWp5cGF4Y21wMiJ9.S2-22wqQvv8B0aiya-Mh7A'}
-        mapStyle={'mapbox://styles/gianlu01/ck5z9olku3d2r1jov9drsa1uu'  }
-        >
-        <div>
-          <input placeholder="Cerca un locale"></input>
-        </div>
-        </ReactMapGL>
-        </div>
-      );
-  }
-  const MODES = [
-  { id: EditorModes.EDITING, text: 'Select and Edit Feature'},
-  { id: EditorModes.DRAW_POINT, text: 'Draw Point'},
-  { id: EditorModes.DRAW_PATH, text: 'Draw Polyline'},
-  { id: EditorModes.DRAW_POLYGON, text: 'Draw Polygon'},
-  { id: EditorModes.DRAW_RECTANGLE, text: 'Draw Rectangle'}
-];
-*/
 const Map = ReactMapboxGl({
   accessToken:
     "pk.eyJ1IjoiZ2lhbmx1MDEiLCJhIjoiY2s1ejQ0a2gyMDY5NjNtcWp5cGF4Y21wMiJ9.S2-22wqQvv8B0aiya-Mh7A"
@@ -54,6 +21,15 @@ export default function App() {
     console.log(features);
   };
 
+  const controls = {
+    polygon: true,
+    trash: true,
+    point: false,
+    line_string: false,
+    combine_features:false,
+    uncombine_features:false
+  }
+
   return (
     <div>
       <Map
@@ -64,7 +40,7 @@ export default function App() {
         }}
         center={[9.19,45.466944]}
       >
-        <DrawControl onDrawCreate={onDrawCreate} onDrawUpdate={onDrawUpdate} />
+        <DrawControl onDrawCreate={onDrawCreate} onDrawUpdate={onDrawUpdate} controls={controls} />
       </Map>
     </div>
   );
