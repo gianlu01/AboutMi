957d26ea5a224fb3a352aa3b4fdd2a34d3a0069b
Michele Banfi <michi.banfi01@gmail.com>
2020-01-31 10:51:24 +0100

    implemented class

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -8,67 +8,79 @@ import * as turf from '@turf/turf';
 //accessToken: 'pk.eyJ1IjoiZ2lhbmx1MDEiLCJhIjoiY2s1ejQ0a2gyMDY5NjNtcWp5cGF4Y21wMiJ9.S2-22wqQvv8B0aiya-Mh7A';
 //site URL: mapbox://styles/gianlu01/ck5z9olku3d2r1jov9drsa1uu
 
-var points = turf.points([
-    [9.19,45.466944],
-    [9.1792887,45.4704962]
-]);
-
-var markerState = {
-  state: false,
-  markers: []
-}
-
 const Map = ReactMapboxGl({
-  accessToken:
-    "pk.eyJ1IjoiZ2lhbmx1MDEiLCJhIjoiY2s1ejQ0a2gyMDY5NjNtcWp5cGF4Y21wMiJ9.S2-22wqQvv8B0aiya-Mh7A"
+    accessToken: "pk.eyJ1IjoiZ2lhbmx1MDEiLCJhIjoiY2s1ejQ0a2gyMDY5NjNtcWp5cGF4Y21wMiJ9.S2-22wqQvv8B0aiya-Mh7A"
 });
 
-export default function App() {
-  const onDrawCreate = ({ features }) => {
-    var poly = features[0].geometry.coordinates;
-    poly = turf.polygon(poly);
-    var result = turf.pointsWithinPolygon(points, poly);
-    if (result.features.length <= 0) {
-      alert("Locals not founds")
-    }else{
-      markerState.state = true
-      markerState.markers = result.features;
+class Maps extends React.Component{
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        stato: false,
+        markers: []
+      }
     }
-  };
+    render (){
+      var points = turf.points([
+          [9.19,45.466944],
+          [9.1792887,45.4704962]
+      ]);
+      const controls = {
+        polygon: true,
+        trash: true,
+        point: false,
+        line_string: false,
+        combine_features:false,
+        uncombine_features:false
+      }
+        const onDrawCreate = ({ features }) => {
+            var poly = features[0].geometry.coordinates;
+            poly = turf.polygon(poly);
+            var result = turf.pointsWithinPolygon(points, poly);
+            if (result.features.length <= 0) {
+                alert("Locals not founds")
+            }else{
+              this.setState({stato: true});
+              this.setState({markers: result.features});
+            }
+          };
 
-  const onDrawUpdate = ({ features }) => {
-    console.log(features);
-  };
+          const onDrawUpdate = ({ features }) => {
+            console.log(features);
+          };
+          if(this.state.stato){
+      return(
+        <div>
+          <Map
+            style=  "mapbox://styles/mapbox/streets-v9" // eslint-disable-line
+            containerStyle={{
+              height: "100vh",
+              width: "100%"
+            }}
+            center={[9.19,45.466944]}
+            >
+            //@TODO pasto sai cosa devi fare ahahaha
+            <div style={{textAlign: 'center'}}>
+              <input type='text' placeholder='ahhhhhhhhh' style={{position: 'absolute'}}></input>
+              </div>
+              <DrawControl onDrawCreate={onDrawCreate} onDrawUpdate={onDrawUpdate} controls={controls} />
+                  <Marker coordinates={this.state.markers} />
+              </Map>
+              </div>
+            );
+          }else{
+            return(
 
-  const controls = {
-    polygon: true,
-    trash: true,
-    point: false,
-    line_string: false,
-    combine_features:false,
-    uncombine_features:false
-  }
-
-  return (
-    <div>
-      <Map
-        style="mapbox://styles/mapbox/streets-v9" // eslint-disable-line
-        containerStyle={{
-          height: "100vh",
-          width: "100%"
-        }}
-        center={[9.19,45.466944]}
-      >
-      //@TODO pasto sai cosa devi fare ahahaha
-        <div style={{textAlign: 'center'}}>
-          <input type='text' placeholder='ahhhhhhhhh' style={{position: 'absolute'}}></input>
-        </div>
-        <DrawControl onDrawCreate={onDrawCreate} onDrawUpdate={onDrawUpdate} controls={controls} />
-
-      </Map>
-    </div>
-  );
+            );
+          }
+          }
 }
+
+export default Maps;
+
+
+
 /*
 {if (markerState.state){
   <Marker
