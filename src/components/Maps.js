5b8336383e55e324e90c8b0367b19429948fbe1a
Michele Banfi <michi.banfi01@gmail.com>
2020-01-31 10:15:27 +0100

    turf library works clean

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ReactMapboxGl, {Layer} from "react-mapbox-gl";
+import ReactMapboxGl, {Layer, Marker} from "react-mapbox-gl";
 import DrawControl from "react-mapbox-gl-draw";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+import * as turf from '@turf/turf';
 
 //accessToken: 'pk.eyJ1IjoiZ2lhbmx1MDEiLCJhIjoiY2s1ejQ0a2gyMDY5NjNtcWp5cGF4Y21wMiJ9.S2-22wqQvv8B0aiya-Mh7A';
 //site URL: mapbox://styles/gianlu01/ck5z9olku3d2r1jov9drsa1uu
+
+var points = turf.points([
+    [9.19,45.466944],
+    [9.1792887,45.4704962]
+]);
+
+var markerState = {
+  state: false,
+  markers: []
+}
 
 const Map = ReactMapboxGl({
   accessToken:
@@ -14,7 +25,15 @@ const Map = ReactMapboxGl({
 
 export default function App() {
   const onDrawCreate = ({ features }) => {
-    console.log(features);
+    var poly = features[0].geometry.coordinates;
+    poly = turf.polygon(poly);
+    var result = turf.pointsWithinPolygon(points, poly);
+    if (result.features.length <= 0) {
+      alert("Locals not founds")
+    }else{
+      markerState.state = true
+      markerState.markers = result.features;
+    }
   };
 
   const onDrawUpdate = ({ features }) => {
@@ -41,11 +60,19 @@ export default function App() {
         center={[9.19,45.466944]}
       >
       //@TODO pasto sai cosa devi fare ahahaha
-      <div style={{textAlign: 'center'}}>
-        <input type='text' placeholder='ahhhhhhhhh' style={{position: 'absolute'}}></input>
+        <div style={{textAlign: 'center'}}>
+          <input type='text' placeholder='ahhhhhhhhh' style={{position: 'absolute'}}></input>
         </div>
         <DrawControl onDrawCreate={onDrawCreate} onDrawUpdate={onDrawUpdate} controls={controls} />
+
       </Map>
     </div>
   );
 }
+/*
+{if (markerState.state){
+  <Marker
+    coordinates={[-0.2416815, 51.5285582]}
+    anchor="bottom">
+  </Marker>
+}}*/
